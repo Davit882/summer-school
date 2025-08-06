79c52529e1934d77ae467d8121f4f68a1c781eca
Davit882 <harutyunyan152c@gmail.com>
2025-08-06 12:43:58 +0400

    array.cpp

--- array.cpp
+++ array.cpp
@@ -1,53 +1,45 @@
 #include <iostream>
 
-const int SIZE = 10;
 
-
-void inputArray(int arr[]) {
-    std::cout << "Enter " << SIZE << " integers:\n";
-    for (int i = 0; i < SIZE; ++i) {
-        std::cout << "Element #" << (i + 1) << ": ";
-        std::cin >> arr[i];
-    }
-}
-
-
-bool removeFirstOccurrence(int arr[], int value) {
-    for (int i = 0; i < SIZE; ++i) {
+void removeFirstOccurrence(int arr[], int size, int value) {
+    for (int i = 0; i < size; i++) {
         if (arr[i] == value) {
-            for (int j = i; j < SIZE - 1; ++j) {
+          
+            for (int j = i; j < size - 1; j++) {
                 arr[j] = arr[j + 1];
             }
-            arr[SIZE - 1] = 0; 
-            return true; 
+            arr[size - 1] = 0; 
+            break; 
         }
     }
-    return false; 
 }
 
-void printArray(int arr[]) {
-    std::cout << "\n Final array after removal:\n[ ";
-    for (int i = 0; i < SIZE; ++i) {
+void printArray(int arr[], int size) {
+    std::cout << "Final array: ";
+    for (int i = 0; i < size; i++) {
         std::cout << arr[i] << " ";
     }
-    std::cout << "]\n";
+    std::cout << "\n";
 }
 
 int main() {
+    const int SIZE = 10;
     int arr[SIZE];
-    int N;
 
-    inputArray(arr);
-
-    std::cout << "\nEnter the value to remove: ";
-    std::cin >> N;
-
-    bool removed = removeFirstOccurrence(arr, N);
-    if (!removed) {
-        std::cout << "Value not found in array. Nothing was removed.\n";
+  
+    for (int i = 0; i < SIZE; i++) {
+        std::cout << "Enter element " << i + 1 << ": ";
+        std::cin >> arr[i];
     }
 
-    printArray(arr);
+    int N;
+    std::cout << "Enter the value to remove: ";
+    std::cin >> N;
+
+   
+    removeFirstOccurrence(arr, SIZE, N);
+
+    printArray(arr, SIZE);
 
     return 0;
 }
