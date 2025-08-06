73453a0c6a1f9d73739eb6da484e9258a0aea07d
Davit882 <harutyunyan152c@gmail.com>
2025-08-06 11:44:11 +0400

    calc.cpp

--- calc.cpp
+++ calc.cpp
@@ -6,32 +6,32 @@ int main() {
     char numericalFunction;
     int num2;
 
-    std::cout << "Hello, please enter your first number ";
+    std::cout << "Hello, please enter your first number: ";
     std::cin >> num1; 
 
-    std::cout << "Hello, please enter numerical function";
+    std::cout << "Hello, please enter numerical function: ";
     std::cin >> numericalFunction;
     
-     std::cout << "Hello, please enter your second number ";
+    std::cout << "Hello, please enter your second number: ";
     std::cin >> num2;
 
-    switch(numericalFunction){
+    switch (numericalFunction) {
         case '+':
-        std::cout<< num1 + num2;
-        break;
+            std::cout << num1 + num2;
+            break;
         case '-':
-        std::cout<< num1 - num2;
-        break;
+            std::cout << num1 - num2;
+            break;
         case '*':
-        std::cout<< num1 * num2;
-        break;
+            std::cout << num1 * num2;
+            break;
         case '/':
-        if(num2 == 0){
-            std::cout<< "You cannot devide by 0";
-        }else{
-             std::cout<< num1/num2;
-        }
-        break;
+            if (num2 == 0) {
+                std::cout << "You cannot divide by 0";
+            } else {
+                std::cout << num1 / num2;
+            }
+            break;
         default:
             std::cout << "Unknown operator!";
     }
