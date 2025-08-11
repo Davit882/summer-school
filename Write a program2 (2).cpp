a1a5eeb78f46a3476bd7fdc67c68af595fdf24cd
Davit882 <harutyunyan152c@gmail.com>
2025-08-11 15:24:24 +0400

    Update Write a program2 (2).cpp

--- Write a program2 (2).cpp
+++ Write a program2 (2).cpp
@@ -6,13 +6,13 @@ int main() {
     std::cout << "Enter a number: ";
     std::cin >> num;
 
-    // 4 bits representation
+
     const int BITS = 4;
-    // Mask to get only the last 4 bits
     int masked = num & ((1 << BITS) - 1);
 
     std::bitset<BITS> binary(masked);
     std::cout << "4-bit binary: " << binary << std::endl;
 
     return 0;
+
 }
